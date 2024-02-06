5e1e1447b4c610628af73a4158e826ec7bf9e35d
shuja-git <shuja.devops@gmail.com>
2024-02-06 22:34:09 +0530

    fixed id

--- learn-terraform/vpc/main.tf
+++ learn-terraform/vpc/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "project-vpc" {
 }
 
 resource "aws_subnet" "Public-Subnet" {
-  vpc_id     = aws_vpc.project-vpc
+  vpc_id     = aws_vpc.project-vpc.id
   cidr_block = "10.0.0.0/24"
   map_public_ip_on_launch = true
   availability_zone = "us-east-1d"
@@ -18,7 +18,7 @@ resource "aws_subnet" "Public-Subnet" {
 }
 
 resource "aws_subnet" "Private-Subnet" {
-  vpc_id     = aws_vpc.project-vpc
+  vpc_id     = aws_vpc.project-vpc.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "us-east-1d"
